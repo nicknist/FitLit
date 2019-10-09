6a62ecde77a7ac0e1832a105f3c9e6c6abdbe992
Nick Nist <nicknistoc@gmail.com>
2019-10-09 15:11:16 -0600

    Add dope stuff to the page

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+window.addEventListener('load', loadDashBoard);
+
+function loadDashBoard() {
+  let instantiatedUsers = userData.map((user) => new User(user));
+  let userRepo = new UserRepository(instantiatedUsers);
+  updateUserInfo(userRepo.returnUserData(20), userRepo.calculateAvgStepGoal());
+}
+
+function updateUserInfo(userObj, avgStepGoal) {
+  $('#welcome-message').text(userObj.name);
+  $('#user-id').text(userObj.id);
+  $('#user-name').text(userObj.name);
+  $('#user-address').text(userObj.address);
+  $('#user-email').text(userObj.email);
+  $('#user-stride').text(userObj.strideLength);
+  $('#user-step-goal').text(userObj.dailyStepGoal);
+  $('#user-friends').text(userObj.friends);
+  $('#user-compare-goal').text(`Your step goal is ${userObj.dailyStepGoal} and the average is ${avgStepGoal}`)
+}
