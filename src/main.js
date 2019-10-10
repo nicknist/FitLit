15ab6d491fe2cb2dac7d62b8a7d75401d290773c
Dustin Mikusko <dustinjm713@yahoo.com>
2019-10-09 23:21:57 -0600

    Update styling for dashboard page

--- src/main.js
+++ src/main.js
@@ -9,14 +9,14 @@ function loadDashBoard() {
 
 function updateUserInfo(userObj, avgStepGoal) {
   $('#welcome-message').text(userObj.name);
-  $('#user-id').text(userObj.id);
-  $('#user-name').text(userObj.name);
-  $('#user-address').text(userObj.address);
-  $('#user-email').text(userObj.email);
-  $('#user-stride').text(userObj.strideLength);
-  $('#user-step-goal').text(userObj.dailyStepGoal);
-  $('#user-friends').text(userObj.friends);
-  $('#user-compare-goal').text(`Your step goal is ${userObj.dailyStepGoal} and the average is ${avgStepGoal}`)
+  $('#user-id').html(`User ID:<br> ${userObj.id}<br><br>`);
+  $('#user-name').html(`Name:<br> ${userObj.name}<br><br>`);
+  $('#user-address').html(`Address:<br> ${userObj.address}<br><br>`);
+  $('#user-email').html(`Email:<br> ${userObj.email}<br><br>`);
+  $('#user-stride').html(`Stride Length:<br> ${userObj.strideLength}<br><br>`);
+  $('#user-step-goal').html(`Daily Step Goal:<br> ${userObj.dailyStepGoal}<br><br>`);
+  $('#user-friends').html(`Friends:<br> ${userObj.friends}<br><br>`);
+  $('#user-compare-goal').html(`Your step goal is ${userObj.dailyStepGoal} and the average is ${avgStepGoal}`)
 }
 
 function updateHydrationInfo() {
