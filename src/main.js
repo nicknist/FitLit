a79b3bc9c0025cb9ad6d078624ef5ac04b3a0b15
Dustin Mikusko <dustinjm713@yahoo.com>
2019-10-10 10:01:50 -0600

    Add nav menu toggle

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', loadDashBoard);
+document.querySelector('#menu-btn').addEventListener('click', toggleNavMenu);
 
 function loadDashBoard() {
   let instantiatedUsers = userData.map((user) => new User(user));
@@ -9,16 +10,28 @@ function loadDashBoard() {
 
 function updateUserInfo(userObj, avgStepGoal) {
   $('#welcome-message').text(userObj.name);
-  $('#user-id').html(`User ID:<br> ${userObj.id}<br><br>`);
-  $('#user-name').html(`Name:<br> ${userObj.name}<br><br>`);
-  $('#user-address').html(`Address:<br> ${userObj.address}<br><br>`);
-  $('#user-email').html(`Email:<br> ${userObj.email}<br><br>`);
-  $('#user-stride').html(`Stride Length:<br> ${userObj.strideLength}<br><br>`);
-  $('#user-step-goal').html(`Daily Step Goal:<br> ${userObj.dailyStepGoal}<br><br>`);
-  $('#user-friends').html(`Friends:<br> ${userObj.friends}<br><br>`);
+  $('#user-id').html(`User ID:<br><br> ${userObj.id}<br>`);
+  $('#user-name').html(`Name:<br><br> ${userObj.name}<br>`);
+  $('#user-address').html(`Address:<br><br> ${userObj.address}<br>`);
+  $('#user-email').html(`Email:<br><br> ${userObj.email}<br>`);
+  $('#user-stride').html(`Stride Length:<br><br> ${userObj.strideLength}<br>`);
+  $('#user-step-goal').html(`Daily Step Goal:<br><br> ${userObj.dailyStepGoal}<br>`);
+  $('#user-friends').html(`Friends:<br><br> ${userObj.friends}<br>`);
   $('#user-compare-goal').html(`Your step goal is ${userObj.dailyStepGoal} and the average is ${avgStepGoal}`)
 }
 
 function updateHydrationInfo() {
   
+}
+
+function toggleNavMenu(event) {
+  if (document.querySelector('nav').classList.contains('hidden')) {
+    document.querySelector('nav').classList.remove('hidden');
+    $('.menu-div1').css('margin-right', '40px');
+    $('.menu-div3').css('margin-right', '0px');
+  } else {
+    document.querySelector('nav').classList.add('hidden');
+    $('.menu-div1').css('margin-right', '0px');
+    $('.menu-div3').css('margin-right', '40px');
+  }
 }
